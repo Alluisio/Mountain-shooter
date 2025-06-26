1bb3130c0c5a2c735072c5e7d41582e2e5e3a18e
Alluisio Silva <alluisio.silva@ipdec.org>
2025-06-25 22:06:00 -0400

    feat: creating main loop
    connecting Menu and Game class

---
+++ code/Player.py
@@ -0,0 +1,12 @@
+from Entity import Entity
+
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+
+    def update(self):
+        pass
+
+    def move(self):
+        pass
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import pygame
+from code.Game import Game
 
-pygame.init()
-win = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Mountain Shooter")
-
-while True:
-    # check events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+game = Game()
+game.run()
